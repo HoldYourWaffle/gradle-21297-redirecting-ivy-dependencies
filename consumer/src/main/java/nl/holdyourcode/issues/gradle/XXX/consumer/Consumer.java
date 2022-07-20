cc365a22d9e84e6360a548fad7abd6f83371eeb1
HoldYourWaffle <ravivanrooijen@live.nl>
2022-07-20 05:32:14 +0200

    Configure (broken) consumer of makeshift Ivy repository

---
+++ consumer/src/main/java/nl/holdyourcode/issues/gradle/XXX/consumer/Consumer.java
@@ -0,0 +1,11 @@
+package nl.holdyourcode.issues.gradle.XXX.consumer;
+
+import nl.holdyourcode.issues.gradle.XXX.library.LibraryClass;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+        new LibraryClass().hello();
+    }
+
+}
